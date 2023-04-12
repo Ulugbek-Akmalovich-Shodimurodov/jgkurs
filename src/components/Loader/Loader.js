615c9bc32d8b0a68adf43c06b68729ca9513ab8b
Ulugbek-Akmalovich-Shodimurodov <99031894+Ulugbek-Akmalovich-Shodimurodov@users.noreply.github.com>
2023-04-12 21:36:33 +0500

    qwertyu

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {ClimbingBoxLoader} from 'react-spinners';
+import { ClimbingBoxLoader } from 'react-spinners';
 import './loader.css'
 
 function Loader() {
   return (
     <div className='loader'>
       <ClimbingBoxLoader />
-      <h3>LOADING</h3>
     </div>
   );
 }
